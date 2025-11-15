88f956f8cb295891124f05899a5440057d32592f
Kanishk Saravanan <kanishk.saravanan@outlook.com>
2025-11-15 19:09:52 +1100

    Added comments on what each drawing means

--- simulate.py
+++ simulate.py
@@ -14,10 +14,14 @@ yellow = (255, 255, 0)
 
 pitch.fill(green)
 
+#Pitch markings
+
 pygame.draw.rect(pitch, white, pygame.Rect(250, 250, 1930, 1320), 50)
 pygame.draw.circle(pitch, black, (1215, 910), 10)
 pygame.draw.circle(pitch, black, (1215, 610), 10)
 pygame.draw.circle(pitch, black, (1215, 1210), 10)
+
+#Goal Boxes
 
 pygame.draw.line(pitch, black, (300, 460), (610, 460), 20)
 pygame.draw.line(pitch, black, (600, 460), (600, 1370), 20)
@@ -25,6 +29,8 @@ pygame.draw.line(pitch, black, (600, 1360), (300, 1360), 20)
 pygame.draw.line(pitch, black, (2130, 460), (1820, 460), 20)
 pygame.draw.line(pitch, black, (1830, 460), (1830, 1370), 20)
 pygame.draw.line(pitch, black, (1830, 1360), (2130, 1360), 20)
+
+#Goals
 
 pygame.draw.rect(pitch, cyan, pygame.Rect(226, 685, 74, 450))
 pygame.draw.rect(pitch, yellow, pygame.Rect(2130, 685, 74, 450))
