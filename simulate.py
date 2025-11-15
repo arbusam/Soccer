ccb443250a298e35dc60f6071ecd1f9c87360bae
Arhan Busam <arhan.busam@gmail.com>
2025-11-15 17:19:05 +1100

    Simulate created and renders pitch

---
+++ simulate.py
@@ -0,0 +1,49 @@
+import pygame
+
+pygame.init()
+
+display = pygame.display.set_mode((1215, 910))
+
+pitch = pygame.Surface((2430, 1820))
+
+green = (20, 110, 44)
+white = (255, 255, 255)
+black = (0, 0, 0)
+cyan = (0, 255, 255)
+yellow = (255, 255, 0)
+
+pitch.fill(green)
+
+pygame.draw.rect(pitch, white, pygame.Rect(250, 250, 1930, 1320), 50)
+pygame.draw.circle(pitch, black, (1215, 910), 10)
+pygame.draw.circle(pitch, black, (1215, 610), 10)
+pygame.draw.circle(pitch, black, (1215, 1210), 10)
+
+pygame.draw.line(pitch, black, (300, 460), (610, 460), 20)
+pygame.draw.line(pitch, black, (600, 460), (600, 1370), 20)
+pygame.draw.line(pitch, black, (600, 1360), (300, 1360), 20)
+pygame.draw.line(pitch, black, (2130, 460), (1820, 460), 20)
+pygame.draw.line(pitch, black, (1830, 460), (1830, 1370), 20)
+pygame.draw.line(pitch, black, (1830, 1360), (2130, 1360), 20)
+
+pygame.draw.rect(pitch, cyan, pygame.Rect(226, 685, 74, 450))
+pygame.draw.rect(pitch, yellow, pygame.Rect(2130, 685, 74, 450))
+pygame.draw.line(pitch, black, (300, 685), (222, 685), 10)
+pygame.draw.line(pitch, black, (226, 685), (226, 1140), 10)
+pygame.draw.line(pitch, black, (226, 1135), (300, 1135), 10)
+pygame.draw.line(pitch, black, (2130, 685), (2208, 685), 10)
+pygame.draw.line(pitch, black, (2204, 685), (2204, 1140), 10)
+pygame.draw.line(pitch, black, (2204, 1135), (2130, 1135), 10)
+
+
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    scaled = pygame.transform.smoothscale(pitch, (1215, 910))
+    display.blit(scaled, (0, 0))
+    pygame.display.flip()
+    clock.tick(60)
