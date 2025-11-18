ced9c65e87adbce4fce06cccea11a73cc37671bb
Arhan Busam <arhan.busam@gmail.com>
2025-11-19 10:58:45 +1100

    Add defence.py stucture and simulate defence logic in simulate.py

---
+++ defence.py
@@ -0,0 +1,13 @@
+def move(direction, speed): # degrees, mm/s
+    # TODO: Move the robot in the given direction at the given speed
+    pass
+
+def defence(x_pos, y_pos, yaw, ball_x, ball_y):
+    return 0, 500 # Move forward
+
+def goalie(x_pos, y_pos, yaw, ball_x, ball_y):
+    return 0, 0 # Do nothing
+
+if __name__ == "__main__":
+    # TODO: Get the x_pos, y_pos, yaw, ball_x, ball_y from the sensors
+    pass
