aed09e6958d785203311e3fd92515cd523e08201
ML3240 <117814397+ML3240@users.noreply.github.com>
2025-11-20 16:16:59 +1100

    added simple defense code to follow the ball

--- defence.py
+++ defence.py
@@ -1,14 +1,22 @@
+import math
 def move(direction, speed): # degrees, mm/s
     # TODO: Move the robot in the given direction at the given speed
     pass
 
 def defence(x_pos, y_pos, yaw, ball_x, ball_y):
-    if x_pos - ball_x < 30 and x_pos - ball_x > -30:
-        if y_pos - ball_y < 30 and y_pos - ball_y > -30:
-            return 0, 0
-        return 90, 500
-    else:
-        return -60, 500
+    vector = (ball_x-x_pos), (ball_y-y_pos)
+    angle = math.degrees(math.atan2(vector[1], vector[0]))
+    dist = math.sqrt(vector[0]**2 + vector[1]**2)
+    print(angle, dist)
+    offset = 0
+    if dist < 180:
+        if angle < 10 and angle > -10:
+            offset = 0
+        elif angle < 180 and angle > 0:
+            offset = 70
+        else:
+            offset = -70
+    return angle + offset, 500
 
 
 def goalie(x_pos, y_pos, yaw, ball_x, ball_y):
