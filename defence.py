c49f3b466d0535c656453e3bf66eac32950836ed
Arhan Busam <arhan.busam@gmail.com>
2025-11-20 21:42:09 +1100

    Added rotation to simulation and yaw output

--- defence.py
+++ defence.py
@@ -3,6 +3,10 @@ def move(direction, speed): # degrees, mm/s
     # TODO: Move the robot in the given direction at the given speed
     pass
 
+# Outputs: direction, speed, rotation
+# direction: degrees to move in
+# speed: mm/s to move at
+# rotation: yaw value to rotate towards
 def defence(x_pos, y_pos, yaw, ball_x, ball_y):
     vector = (ball_x-x_pos), (ball_y-y_pos)
     angle = math.degrees(math.atan2(vector[1], vector[0]))
@@ -16,7 +20,7 @@ def defence(x_pos, y_pos, yaw, ball_x, ball_y):
             offset = 80
         else:
             offset = -80
-    return angle + offset, 500
+    return angle + offset, 500, 0
 
 
 def goalie(x_pos, y_pos, yaw, ball_x, ball_y):
