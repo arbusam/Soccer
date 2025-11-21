c5056c8bfdf67c08194e94ddb9623f221828c1a0
Arhan Busam <arhan.busam@gmail.com>
2025-11-21 20:09:59 +1100

    Add option to simulate goalie code

--- defence.py
+++ defence.py
@@ -1,22 +1,25 @@
 import math
-def move(direction, speed): # degrees, mm/s
+
+
+def move(direction, speed):  # degrees, mm/s
     # TODO: Move the robot in the given direction at the given speed
     pass
+
 
 # Outputs: direction, speed, rotation
 # direction: degrees to move in
 # speed: mm/s to move at
 # rotation: yaw value to rotate towards
 def defence(x_pos, y_pos, yaw, ball_x, ball_y):
-    vector = (ball_x-x_pos), (ball_y-y_pos)
+    vector = (ball_x - x_pos), (ball_y - y_pos)
     angle = math.degrees(math.atan2(vector[1], vector[0]))
-    dist = math.sqrt(vector[0]**2 + vector[1]**2)
+    dist = math.sqrt(vector[0] ** 2 + vector[1] ** 2)
     print(angle, dist)
     offset = 0
     if dist < 200:
-        if angle < 10 and angle > -10:
+        if -10 < angle < 10:
             offset = 0
-        elif angle < 180 and angle > 0:
+        elif 0 < angle < 180:
             offset = 80
         else:
             offset = -80
